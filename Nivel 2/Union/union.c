cfd82a0e28eab867463dec25f4f05723c2773903
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-07-13 22:00:08 -0300

    Update union.c

--- Nivel 2/Union/union.c
+++ Nivel 2/Union/union.c
@@ -1,43 +1,38 @@
 #include <unistd.h>
 
-int check(int c, char *str, int index)
-{
-	int i = 0;
-	
-	while(i < index)
-	{
-		if (str[i] == c)
-			return 0;
-		i++;
-	}
-	return  1;
+static size_t ft_strlen(char const *s) {
+  size_t i = 0;
+
+  while (s[i])
+    ++i;
+  return (i);
 }
 
-int main(int argc, char **argv)
-{	
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	
-	if (argc == 3)
-	{
-		while(argv[1][i] != '\0')
-		{
-			i++;
-		}
-		while(argv[2][j] != '\0')
-		{
-			argv[1][i] = argv[2][j];
-			i++;
-			j++;
-		}
-		i--;
-		while(k <= i)
-		{
-			if(check(argv[1][k], argv[1], k) == 1) 
-				write (1, &argv[1][k], 1); 
-			k++;
-		}
-	}
-	write (1, "\n", 1);
+static int not_double(char const *s, char c, int i) {
+  for (int k = 0; k < i; ++k) {
+    if (s[k] == c)
+      return (0);
+  }
+  return (1);
 }
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    write(STDOUT_FILENO, "\n", 1);
+    return (0);
+  }
+
+  size_t const len = ft_strlen(argv[1]);
+
+  for (int i = 0; argv[1][i]; ++i)
+    if (not_double(argv[1], argv[1][i], i))
+      write(STDOUT_FILENO, &argv[1][i], 1);
+
+  for (int i = 0; argv[2][i]; ++i)
+    if (not_double(argv[1], argv[2][i], len))
+      if (not_double(argv[2], argv[2][i], i))
+        write(STDOUT_FILENO, &argv[2][i], 1);
+
+  write(STDOUT_FILENO, "\n", 1);
+  return (0);
+} //codigo by matheuzim, obrigado matheuzim ;)
